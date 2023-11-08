a2e75d776d31dd5970f3829bd8cbc7ed2dfa3d16
Segniko <segniassaye2@gmail.com>
2023-11-08 13:26:30 -0800

    update

--- functions.c
+++ functions.c
@@ -1,8 +1,11 @@
 #include "shell.h"
 
 /**
- * _putchar - writes the character c to stdout 
- * @c: The character to print 
+ * _putchar - writes the character c to stdout
+ *@c:The character to print
+ *
+ * Return: on success 1.
+ * On error, -1 is returned and erno is set appropriately
  */
 
 int _putchar(char c)
@@ -13,7 +16,8 @@ int _putchar(char c)
 int _printstring(char *str)
 {
 	int i = 0, count = 0;
-	while(str[i])
+
+	while (str[i])
 	{
 		count += _putchar(str[i++]);
 	}
